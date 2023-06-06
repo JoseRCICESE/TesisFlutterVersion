ceb2eb23ada0817a6d6cfdbae3e43dccb68980b6
Ember96 <gnomeaddict96@gmail.com>
2023-06-06 03:03:00 -0700

    showing images acepted per user

--- lib/per_stats.dart
+++ lib/per_stats.dart
@@ -1,6 +1,7 @@
 import 'package:TRHEAD/personalized_widgets.dart';
 import 'package:TRHEAD/web3.utils.dart';
 import 'package:TRHEAD/storage.dart';
+import 'package:TRHEAD/classified_image.dart';
 import 'package:flutter/material.dart';
 import 'package:share_plus/share_plus.dart';
 
@@ -12,12 +13,15 @@ class Stats extends StatefulWidget {
 class _StatsState extends State<Stats> {
   String uuid = "";
   String records = "";
+  List<ClassifiedImage> imagesList = [];
 
   @override
   void initState() {
     Web3Utils().getRecords().then((value) {
       setState(() {
         records = value.toString();
+        imagesList = value.map((e) => ClassifiedImage(e[0].toString(), e[1].toString(), e[2].toString(), e[3].toString(), e[4].toString())).toList();
+        print(imagesList[0].cid);
       });
     });
     FileStorage().readFromFile("uuid").then((value) {
@@ -46,15 +50,37 @@ class _StatsState extends State<Stats> {
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Container(
-            margin: EdgeInsets.all(25),
-            child: BigCard(text: "Tu uuid es: $uuid")
+            margin: EdgeInsets.all(15),
+            child: Text("Tu uuid es: $uuid"),
             ),
           SizedBox(
-            height: 15,
+            height: 5,
           ),
           ElevatedButton(
             onPressed: _sharing,
             child: Text('Compartir'),
+          ),
+          SizedBox(
+            height: 25,
+          ),
+          SingleChildScrollView(
+            child: Container(
+              margin: EdgeInsets.all(5),
+              height: 550,
+              child: ListView.separated(        // To add separation line between the ListView 
+                separatorBuilder: (context, index) => Divider(
+                  color: Colors.green,
+                ),
+                  itemCount: imagesList.length,
+                  itemBuilder: (BuildContext context, int index){
+                  return ListTile(
+                    leading: Icon(Icons.image),
+                    trailing: Text("Imagen:"),
+                    title: Text(imagesList[index].name),
+                  );
+                },
+                    ),
+            ),
           ),
         ],
       ),
